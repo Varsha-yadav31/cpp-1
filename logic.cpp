379491320524a3d27110286910f8d6a4d493761b
varsha <152709598+Varsha-yadav31@users.noreply.github.com>
2026-01-30 16:53:09 +0530

    initial commit with source files

---
+++ logic.cpp
@@ -0,0 +1,15 @@
+//Given a number n, check whether it is even or odd. Return true for even and false for odd.
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    if(n%2==0){
+        cout << "even" <<endl;
+        cout << "true"<<endl;
+    }else{
+        cout << "odd"<<endl;
+        cout<<"false"<<endl;
+        
+    }
+}
